f2b55b39aaebed8dca6d1a496954926810c3c11c
rishabhjhs <rishabhagarwaljhs@gmail.com>
2018-10-05 10:39:48 +0530

    code updated with lateral crashing
    this code contains lateral crashing logic and it also has all the functions which were asked to include.

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 var obsticaleLane = (Math.random() * 10) % 3;
-var SPEED = 50;
+var Speed = 50;
 var flag=false;
+var speedFortyFive=45;
+var speedThirtyFive=35;
+var speedTwentyFive=25;
+var speedFive=5;
+var posSeventySeven=77;
+var posNintyThree=93;
+var posEleven=11;
+var posEight=8;
+var posSeven=7;
+var posOne=1;;
+var posTwentyOne=21;
+var posTen=10;
+var posEighteen=18;
+var posSeventeen=17;
+var posTwenty=20;
+var facSeventyFive=75;
+var facEighty=80;
+var facHundred=100;
+var facThreeHundred=300;
+var posTwelve=12;
+var leftLane="5vw";
+var middleLane="15vw";
+var lastLane="25vw";
 var moveIntervalId = null;
 var speedIntervalId = null;
-var generatecarId=null;
-
+var generateCarId=null;
+var counter=0;
 function getObstacleLane()
         {
           console.log(parseInt(Math.random() * 10) % 3 );
         }
-
 function handleKey(e){
     if(e.keyCode === 39){
         resetMoveInterval();
@@ -23,12 +45,12 @@ function handleKey(e){
     }
 
     if(e.keyCode == 40){
-        resetSpeed(SPEED+5);
+        resetSpeed(Speed+5);
         return
     }
 
     if(e.keyCode == 38){
-        resetSpeed(SPEED-5);
+        resetSpeed(Speed-5);
         return
     }
 
@@ -40,8 +62,8 @@ var obstacles =document.getElementsByClassName("obstacle");
 
   var pos = 0;
 
-  initSeperatorSpots(elemA)
-  initSeperatorSpots(elemB)
+  initSeperatorSpots(elemA);
+  initSeperatorSpots(elemB);
 
   function frame() {
     if (pos == 10) {
@@ -95,8 +117,6 @@ function moveCarLeft(){
     }
 }
 
-
-
 function carMoveAnimation(els, toPostion){
       var currentPostion = parseFloat(els.style.left || 0);
 
@@ -108,7 +128,7 @@ function carMoveAnimation(els, toPostion){
               return
             }
             els.style.left = (currentPostion + delta) + "vw";
-      }, parseInt(SPEED*0.5)) :moveIntervalId;
+      }, parseInt(Speed*0.5)) :moveIntervalId;
 }
 
 function resetMoveInterval(){
@@ -116,43 +136,42 @@ function resetMoveInterval(){
     moveIntervalId = null;
 }
 
-function resetSpeed(speed)
+function resetSpeed(s)
     {
       clearInterval(speedIntervalId);
-      SPEED =speed;
-      console.log(SPEED);
-      speedIntervalId = setInterval(frame, SPEED);
-      clearInterval(generatecarId);                               
-      if(SPEED>=35 && SPEED <=45){
-      generatecarId=setInterval(genetateOponent,SPEED*100);      /*checking if the speed is in this particular range then the setInterval method is called 
+      Speed =s;
+      console.log(Speed);
+      speedIntervalId = setInterval(frame, Speed);
+      clearInterval(generateCarId);                               
+      if(Speed>=speedTwentyFive && Speed <=speedFortyFive){
+      generateCarId=setInterval(genetateOponent,Speed*facHundred);      /*checking if the speed is in this particular range then the setInterval method is called 
                                                                 and generateOponent fxn is passed which will randomly generate cars on random lanes.
                                                                 second argument is passed in reference with the speed so as to maintain the interval of 
                                                                 generated cars accordingly with speed*/                          
     }
-else if(SPEED>=25 && SPEED<35)
+else if(Speed>=speedTwentyFive && Speed<speedThirtyFive)
     {
-      generatecarId=setInterval(genetateOponent,SPEED*75);
+      generateCarId=setInterval(genetateOponent,Speed*facSeventyFive);
     }
-else if(SPEED>=5 && SPEED<25)
+else if(Speed>=speedFive && Speed<speedTwentyFive)
     {
-      generatecarId=setInterval(genetateOponent,SPEED*80);
+      generateCarId=setInterval(genetateOponent,Speed*facEighty);
     }
-else if(SPEED<5)
+else if(Speed<speedFive)
     {
-      generatecarId=setInterval(genetateOponent,300);          //as there is no threshold value of speed that we increase so if speed is below 5  then interval
-                                                             //is called after every 300 milliseconds.
+      generateCarId=setInterval(genetateOponent,facThreeHundred);         //as there is no threshold value of speed that we increase so if speed is below 5  then interval
+                                                                          //is called after every 300 milliseconds.
     }
 else
     {
-      generatecarId=setInterval(genetateOponent,SPEED*100);   // this condition is written so as to handle the event like if a user starts the game by a down
-                                                            // arrow key.            
+      generateCarId=setInterval(genetateOponent,Speed*facHundred);  // this condition is written so as to handle the event like if a user starts the game by a down                                                        // arrow key.            
     }
 }
 
 function oponentMoveAnimation(el,lane){
+    ++counter;
     var cloneEl = el.cloneNode(true);
     cloneEl.id = cloneEl.id+Date.now();
-    //console.log("running");
     el.parentElement.appendChild(cloneEl);
 
       var pos = 0;
@@ -162,75 +181,99 @@ function oponentMoveAnimation(el,lane){
           cloneEl.style.top = pos + "vh";
           cloneEl.style.left = lane;
           cloneEl.style.zIndex = Date.now() % 1000
-          var id_of_car = document.getElementById('player');
-          var car_currentPostion = parseInt(id_of_car.style.left || 1);    //here the by default value of car_currentposition is changed to 1 insted of 0
-         //console.log("car pos"+car_currentPostion);                     //to handle the crash event in case user just starts the game and do not move the car     
-         // console.log("pos"+pos);
+          var idOfCar = document.getElementById('player');
+          var carCurrentPostion = parseInt(idOfCar.style.left || 1);    //here the by default value of car_currentposition is changed to 1 insted of 0
+                                                                        //to handle the crash event in case user just starts the game and do not move the car
 
-          //console.log(lane);
-          if(car_currentPostion==11 && lane=="15vw" && pos==75)          //These if else statements are written to handle the crash event if the above 
-          {                                                             //contition satisfy crash will occur and all the interval will close    
-                console.log("crash occured");
+          var hasCrachOccurred= crashOccurred(carCurrentPostion,lane,pos); //calls a method crashOccurred which check the events when crash can occur.
+            if(hasCrachOccurred)
+             {
                 cloneEl.parentElement.removeChild(cloneEl);
                 clearInterval(oponentMoveIntervalId);
                 oponentMoveIntervalId=null;
-                reset_intervals();
-                flag=true; 
-                alert("GAME OVER");
-                window.location.reload();
-                
-           }
-          else if(car_currentPostion==1 && lane=="5vw" && pos==75)
-           {
-                console.log("crash occured");
-                cloneEl.parentElement.removeChild(cloneEl);
+                resetIntervals(); 
+             }
+             else if(flag)
+              { 
                 clearInterval(oponentMoveIntervalId);
                 oponentMoveIntervalId=null;
-                reset_intervals();
-                flag=true; 
-                alert("GAME OVER");
-                window.location.reload();
-                
-            }
-
-           else if(car_currentPostion==21 && lane=="25vw" && pos==75)
-            {
-                console.log("crash occured");
-                cloneEl.parentElement.removeChild(cloneEl);
-                clearInterval(oponentMoveIntervalId);
-                oponentMoveIntervalId=null;
-                reset_intervals();
-                flag=true;
-                alert("GAME OVER");
-                window.location.reload();
-                
-            }
-              
-           else if(flag)
-              {
-                console.log("executed");
-                clearInterval(oponentMoveIntervalId);
-                oponentMoveIntervalId=null;
-                reset_intervals();
-              }
-
-
-           else if(pos >110){
+                var reset=confirm("crash occurred");
+                if(reset)
+                {
+                  resetGame(cloneEl);
+                  return;
+                 //resetGame function is called that initialise the game;
+                }
+               }                  
+             else if(pos >110)
+             {
               cloneEl.parentElement.removeChild(cloneEl);
               clearInterval(oponentMoveIntervalId);
-          }
-      }, SPEED);
+             }
+      }, Speed);
 
 return;
 }
 
-function reset_intervals()
+function resetIntervals()
 {
+
     clearInterval(speedIntervalId);
     speedIntervalId=null;
-    clearInterval(generatecarId);  
-    generatecarId=null;
+    clearInterval(generateCarId);  
+    generateCarId=null;
 }
+
+function resetGame(cloneEl)                        //this function is used to initialise the game
+{
+cloneEl.parentElement.removeChild(cloneEl);
+resetSpeed(50);
+flag=false;
+resetIntervals();
+}
+
+function crashOccurred(carCurrentPostion,lane,pos)
+{
+    //These if else statements are written to handle the crash event if the above
+    //contition satisfy crash will occur and all the interval will close
+    if(carCurrentPostion==posEleven && lane==middleLane && pos==posSeventySeven)           
+        {                                                                    
+         flag=true;
+          return true; 
+        }
+    else if(carCurrentPostion<=posSeven && lane==leftLane && (pos>=posSeventySeven && pos<=posNintyThree)) 
+        {       
+         flag=true; 
+         return true;
+        } 
+    else if((carCurrentPostion>=posSeven &&carCurrentPostion<=posTen) && lane==middleLane && (pos>=posSeventySeven && pos<=posNintyThree)) 
+        {
+         flag=true; 
+         return true;
+        } 
+    else if((carCurrentPostion<=posEighteen &&carCurrentPostion>=posTwelve) && lane==middleLane && (pos>=posSeventySeven && pos<=posNintyThree)) 
+        {
+         flag=true;
+         return true;;
+        } 
+    else if((carCurrentPostion>=posSeventeen &&carCurrentPostion<=posTwenty) && lane==lastLane && (pos>=posSeventySeven && pos<=posNintyThree)) 
+        {
+         flag=true;
+         return true;
+        }
+            
+    else if(carCurrentPostion==posOne && lane==leftLane && pos==posSeventySeven)
+        {
+         flag=true;
+         return true;       
+        }
+
+    else if(carCurrentPostion==posTwentyOne && lane==lastLane && pos==posSeventySeven)
+        {
+         flag=true;
+         return true;   
+        }
+    }                
 
 function genetateOponent() {
     var lanes = [
